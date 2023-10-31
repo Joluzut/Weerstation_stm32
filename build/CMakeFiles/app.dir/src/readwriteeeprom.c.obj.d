00342fbf5a8523c1aa077e5c6ed5bdeab234eb81
Joluzut” <luukvanzutphen39@gmail.com>
2023-10-31 15:33:47 +0100

    sufficient data storage

---
+++ build/CMakeFiles/app.dir/src/readwriteeeprom.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/app.dir/src/readwriteeeprom.c.obj: \
+ C:\Users\luukv\zephyrproject\zephyr\samples\sensor\eeprom\src\readwriteeeprom.c \
+ C:/Users/luukv/zephyrproject/zephyr/samples/sensor/eeprom/build/zephyr/include/generated/autoconf.h \
+ C:/Users/luukv/zephyrproject/zephyr/include/zephyr/toolchain/zephyr_stdint.h
